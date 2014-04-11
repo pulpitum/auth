1b9aad39b6ec94d64cd2763bd23b2e2b56da6b8d
Rolando <rolando.guedes@3gnt.net>
2014-04-11 15:22:35 +0100

    fix

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	Route::get('/login', array('as' => 'login',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@getLogin'));
 	Route::get('/admin/login', array('as' => 'login',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@getLogin'));
@@ -13,17 +12,4 @@
 	
 	Route::get('/reset', array('as' => 'reset',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@getReset'));
 	Route::post('/reset', array('as' => 'postReset',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@postReset', "before"=>"csrf"));
-=======
-<?php
-	Route::get('/login', array('as' => 'login',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@getLogin'));
-	Route::post('/login', array('as' => 'postLogin',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@postLogin', "before"=>"csrf"));
-
-	Route::get('/logout', array('as' => 'logout',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@getLogout'));
-
-	Route::get('/recover', array('as' => 'recover',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@getRecover'));
-	Route::post('/recover', array('as' => 'postRecover',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@postRecover', "before"=>"csrf"));
-	
-	Route::get('/reset', array('as' => 'reset',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@getReset'));
-	Route::post('/reset', array('as' => 'postReset',  'uses' => 'Pulpitum\Auth\Controllers\IndexController@postReset', "before"=>"csrf"));
->>>>>>> 7e05397e8dd104beba263c8588a3b09fc8b8e11c
 ?>
